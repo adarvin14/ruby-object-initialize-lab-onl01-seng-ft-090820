6d9d6fb0fe2df0bf5173b12101c82a68be18eb9b
Alex Darvin <amdarvin14@gmail.com>
2020-09-20 17:23:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize(dog_name, dog_breed)
+  def initialize(dog_name, dog_breed = "Mutt")
     @name = dog_name
     @breed = dog_breed
   end
  
-  def name=(dog_name, dog_breed)
+  def name=(dog_name, dog_breed = "Mutt")
     @name = dog_name
     @breed = dog_breed
   end
